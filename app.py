641c752f95d79629965bda708c80726656e41612
Noe Osorio <osorionoe@gmail.com>
2024-03-26 11:41:12 -0600

    Add env file

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from dotenv import load_dotenv
 import stripe
 import os
 
+load_dotenv()
+
 app = Flask(__name__)
 CORS(app)
-# TODO: Env vars
 # Configura tu clave secreta de Stripe
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
-
+return_url = os.getenv('RETURN_URL', 'http://localhost:3000/success')
 
 @app.route('/create-payment-intent', methods=['POST'])
 def create_payment():
@@ -23,7 +25,7 @@ def create_payment():
             receipt_email=data['email'],  # Opcional: enviar recibo por email
             # Puedes añadir metadata para almacenar información adicional
             metadata={'name': data['name'], 'email': data['email']},
-            return_url="http://localhost:3000/success",
+            return_url=return_url,
         )
         return jsonify({
             'clientSecret': intent.client_secret
